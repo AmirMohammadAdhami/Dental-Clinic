d8726fb6c44c80cef448ff2615e8afbbd6e560bf
Amir Mohammad Adhami <amiradhamir@gmail.com>
2026-08-21 12:46:01 +0330

    create blog template

---
+++ front-end/blog.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // FAQ accordion
+    var faqBtns = document.querySelectorAll('.faq-question');
+    faqBtns.forEach(function(btn) {
+        btn.addEventListener('click', function() {
+            var item = btn.closest('.faq-item');
+            var isOpen = item.classList.contains('is-open');
+            // Close all
+            document.querySelectorAll('.faq-item').forEach(function(fi) {
+                fi.classList.remove('is-open');
+            });
+            // Toggle current
+            if (!isOpen) {
+                item.classList.add('is-open');
+            }
+        });
+    });
+});
