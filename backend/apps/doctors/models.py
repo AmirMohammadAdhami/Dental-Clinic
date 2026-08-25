1d0d38328f7c0ee6b25a532e00871a5167228a0b
Amir Mohammad Adhami <amiradhamir@gmail.com>
2026-08-25 20:16:38 +0330

    some changes to doctors/models.py: 1- add related name to Doctor.user , DoctorTestimonial.doctor, DoctorPhotos.doctor, Assistant.user 2- add upload_url to DoctorPhotos(profile_photo , blog_photo) 3- create certificate model

--- backend/apps/doctors/models.py
+++ backend/apps/doctors/models.py
@@ -5,7 +5,7 @@ from django.utils.text import slugify
 
 # Create your models here.
 class Doctor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='doctor')
 
     slug = models.SlugField(unique=True)
 
@@ -23,29 +23,37 @@ class Doctor(models.Model):
         if not self.slug:
             self.slug = slugify(f"{self.user.first_name}-{self.user.last_name}", allow_unicode=True)
 
-        super.save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.user.first_name + " " + self.user.last_name
 
 
 class DoctorTestimonial(models.Model):
-    doctor = models.OneToOneField(Doctor, on_delete=models.CASCADE)
+    doctor = models.OneToOneField(Doctor, on_delete=models.CASCADE, related_name='testimonial')
     video = models.FileField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class DoctorPhotos(models.Model):
-    doctor = models.OneToOneField(Doctor, on_delete=models.CASCADE)
-    profile_photo = models.ImageField()
-    blog_photo = models.ImageField()
+    doctor = models.OneToOneField(Doctor, on_delete=models.CASCADE, related_name='photos')
+    profile_photo = models.ImageField(upload_to='doctors/profile_photos/')
+    blog_photo = models.ImageField(upload_to='doctors/blog_photos/')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
+class Certificate(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name='certificates')
+    date = models.DateField()
+    what = models.TextField(max_length=255)
+    where = models.TextField(max_length=255)
+
+
+
 class Assistant(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='assistant')
 
     speciality = models.CharField()
     blog_photo = models.ImageField()
