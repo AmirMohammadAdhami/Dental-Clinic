326b46625a8f623e0e72b335766e0834f1b46f55
Amir Mohammad Adhami <amiradhamir@gmail.com>
2026-09-03 15:33:23 +0330

    some improvement to cache

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -1,7 +1,5 @@
 import os
-
 from celery import Celery
-from backend.apps.appointments.tasks import release_expired_reservations_task, mark_completed_appointments_task
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
@@ -24,5 +22,18 @@ def debug_task(self):
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(300.0, release_expired_reservations_task.s(), name='release expired reservations')
-    sender.add_periodic_task(15*60, mark_completed_appointments_task.s(), name='mark completed appointments')
+    sender.add_periodic_task(
+        300.0,
+        sender.signature(
+            'backend.apps.appointments.tasks.release_expired_reservations_task'
+        ),
+        name='release expired reservations',
+    )
+
+    sender.add_periodic_task(
+        300.0,
+        sender.signature(
+            'backend.apps.appointments.tasks.mark_completed_appointments_task'
+        ),
+        name='mark completed appointments',
+    )
